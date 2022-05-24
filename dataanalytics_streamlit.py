3ebca03d620f8e02d9b9af0aaab2e3376824af9e
AKpython <72851630+akpythonyt@users.noreply.github.com>
2022-05-24 13:26:48 +0530

    Update dataanalytics_streamlit.py

--- dataanalytics_streamlit.py
+++ dataanalytics_streamlit.py
@@ -55,7 +55,7 @@ fig1 = go.Figure(data=[go.Scatter(
 )])
 st.plotly_chart(fig1)
 st.title('Top 5 Bowlers Based on wickets')
-Bowlers=pd.read_excel(r'https://github.com/akpythonyt/dataanalyticsapp/blob/main/Top_100_bowlers.xlsx')
+Bowlers=pd.read_excel(r'https://github.com/akpythonyt/dataanalyticsapp/blob/main/Top_100_bowlers.xlsx%20-%20Sheet1.csv')
 Bowlers_matches = Bowlers[Bowlers['Wkts']>1]
 topfive=(Bowlers_matches['PLAYER'].iloc[0:5])
 fig2 = go.Figure()
